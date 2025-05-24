00c5cd42b379581a3d6fd9cd66771f92eb142ecf
samzinalker <samzinalker@gmail.com>
2025-05-24 08:07:43 -0500

    f

--- sistemadeventas/app/controllers/clientes/listado_de_clientes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$sql_clientes = "SELECT * FROM tb_clientes";
-$query_clientes = $pdo->prepare($sql_clientes);
-$query_clientes->execute();
-$clientes_datos = $query_clientes->fetchAll(PDO::FETCH_ASSOC);
--- sistemadeventas/app/controllers/proveedores/listado_de_proveedores.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$sql_proveedores = "SELECT * FROM tb_proveedores ";
-$query_proveedores = $pdo->prepare($sql_proveedores);
-$query_proveedores->execute();
-$proveedores_datos = $query_proveedores->fetchAll(PDO::FETCH_ASSOC);
